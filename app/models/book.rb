8a6a8252d4c7638a8bdef0d95918b09f0022408b
YOUR_FULL NAME_HERE <your@email.here.com>
2020-10-20 00:00:31 +1100

    test

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ApplicationRecord
   belongs_to :author, :optional =>true
+  has_and_belongs_to_many :gneres
+  has_and_belongs_to_many :shelves
 end
--- app/models/shelf.rb
+++ app/models/shelf.rb
@@ -1,3 +1,4 @@
 class Shelf < ApplicationRecord
   belongs_to :user , :optional =>true
+  has_and_belongs_to_many :books
 end
